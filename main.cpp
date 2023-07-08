f27a4c2c2c42f2f4da49ba5acd4592fb8d317581
kiniikun <kaushal.panduru@gmail.com>
2023-07-08 09:25:22 +0000

    version 1 commit message
    Signed-off-by: kiniikun <kaushal.panduru@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
+// comment on why this has occoured!! 
+
+
 #include "mbed.h"
 
 // main() runs in its own thread in the OS
-DigitalOut led(LED1);
+InterruptIn bttn(PC_13);
+
+void bttn_interrupt(){
+    printf("Buttn pressed \r\n");
+}
 
 int main()
 {
+    bttn.fall(&bttn_interrupt);
     while (true) {
 
     }
